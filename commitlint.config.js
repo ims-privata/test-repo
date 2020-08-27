9235a2eb9fa72d51f82e1d1e2b23049f3d51449e
Ian Smith <ismith@imprivata.com>
2020-08-27 09:29:08 -0400

    zelg: add zelg as a commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2,  'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'ci', 'chore']],
+        'type-enum': [2,  'always', ['zelg','feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'ci', 'chore']],
     },
     ignores: [(commit) => commit === 'squash'],
 };
